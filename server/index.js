023d4900db91d1c9d70a63c46e899d70c2fa162c
Anthony Alarcon <ajpac33@gmail.com>
2025-09-09 18:02:52 -0500

    nueva direccion agregada a index.js en server

--- server/index.js
+++ server/index.js
@@ -13,7 +13,11 @@ const PORT = process.env.PORT || 5000;
 const app = express();
 app.use(
   cors({
-    origin: ["http://localhost:3000", "http://localhost:3001"],
+    origin: [
+      "http://localhost:3000",
+      "http://localhost:3001",
+      "https://ajpac33-task-manager-app.netlify.app",
+    ],
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   })
